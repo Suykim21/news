658744f9cf7bcf1411f64d7e06b72191774c08f0
Steve Kim <suykim21@Steves-MacBook.local>
2017-07-19 14:48:06 -0700

    pulling 7/19 updates

--- apps/news_app/static/news_app/js/index.js
+++ apps/news_app/static/news_app/js/index.js
@@ -2,23 +2,43 @@ $(document).ready(function() {
 
   /* For the sticky navigation */
   $('.js--section-features').waypoint(function(direction) {
-      if (direction == "down") {
-          $('nav').addClass('sticky');
-      } else {
-          $('nav').removeClass('sticky');
-      }
+    if (direction == "down") {
+      $('nav').addClass('sticky');
+    } else {
+      $('nav').removeClass('sticky');
+    }
   }, {
     offset: '60px;'
   });
 
+
+  /* Mobile navigation */
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round');
+    }
+  });
   // click flash it button
   $('.js--scroll-to-latest').click(function() {
-    $('html, body').animate({scrollTop: $('.js--section-news').offset().top}, 1000);
+    $('html, body').animate({
+      scrollTop: $('.js--section-news').offset().top
+    }, 1000);
   });
 
   // click Subscribe  button
   $('.js--scroll-to-subscription').click(function() {
-    $('html, body').animate({scrollTop: $('.js--subscription').offset().top}, 1000);
+    $('html, body').animate({
+      scrollTop: $('.js--subscription').offset().top
+    }, 1000);
   });
 
   // Animations on scroll
