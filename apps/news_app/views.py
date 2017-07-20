0c0963c5b407ac17115937803598110fd97229b0
Jake Kiernan <kiernan.jake@gmail.com>
2017-07-20 14:42:59 -0700

    repackaged API data

--- apps/news_app/views.py
+++ apps/news_app/views.py
@@ -6,7 +6,6 @@ from datetime import datetime
 import random
 
 
-
 def index(request):
   print "*"*50
   images = []
@@ -17,8 +16,10 @@ def index(request):
 
   # Huffington Post API call
   responseH = requests.get('https://newsapi.org/v1/articles?source=the-huffington-post&sortBy=top&apiKey=3b606a8547b24dfdbf43e72388b6f539')
-  #cached HuffPo data
   dataH = responseH.json()
+  Huff = dataH[u'articles']
+  random.shuffle(Huff)  
+  #Delete below
   a = len(dataH)
   b = random.randint(0,a)
   images.append(dataH[u'articles'][b][u'urlToImage'])
@@ -29,91 +30,112 @@ def index(request):
 
   #Entertainment Weekly API
   responseE = requests.get('https://newsapi.org/v1/articles?source=entertainment-weekly&sortBy=top&apiKey=3b606a8547b24dfdbf43e72388b6f539')
-  #cached EW data
   dataE = responseE.json()
-  c = len(dataE)
-  d = random.randint(0,c)
-  images.append(dataE[u'articles'][d][u'urlToImage'])
-  titles.append(dataE[u'articles'][d][u'title'])
-  descriptions.append(dataE[u'articles'][d][u'description'])
-  urls.append(dataE[u'articles'][d][u'url'])
-  pub.append(dataE[u'articles'][d][u'publishedAt'].split('T')[0])
+  EW = dataE[u'articles']
+  random.shuffle(EW) 
+  #Delete below
+  a = len(dataE)
+  b = random.randint(0,a)
+  images.append(dataE[u'articles'][b][u'urlToImage'])
+  titles.append(dataE[u'articles'][b][u'title'])
+  descriptions.append(dataE[u'articles'][b][u'description'])
+  urls.append(dataE[u'articles'][b][u'url'])
+  pub.append(dataE[u'articles'][b][u'publishedAt'].split('T')[0]) 
 
   #Tech Crunch API call
   responseT = requests.get('https://newsapi.org/v1/articles?source=techcrunch&sortBy=top&apiKey=3b606a8547b24dfdbf43e72388b6f539')
-  #cached Tech Crunch data
   dataT = responseT.json()
-  e = len(dataT)
-  print e
-  f = random.randint(0,e)
-  print f
-  images.append(dataT[u'articles'][f][u'urlToImage'])
-  titles.append(dataT[u'articles'][f][u'title'])
-  descriptions.append(dataT[u'articles'][f][u'description'])
-  urls.append(dataT[u'articles'][f][u'url'])
-  pub.append(dataT[u'articles'][f][u'publishedAt'].split('T')[0])
+  Crunch = dataT[u'articles']
+  random.shuffle(Crunch)
+  #Delete below
+  a = len(dataT)
+  b = random.randint(0,a)
+  images.append(dataT[u'articles'][b][u'urlToImage'])
+  titles.append(dataT[u'articles'][b][u'title'])
+  descriptions.append(dataT[u'articles'][b][u'description'])
+  urls.append(dataT[u'articles'][b][u'url'])
+  pub.append(dataT[u'articles'][b][u'publishedAt'].split('T')[0])
 
   #Fox Sports API call
   responseF = requests.get('https://newsapi.org/v1/articles?source=fox-sports&sortBy=top&apiKey=3b606a8547b24dfdbf43e72388b6f539')
-  #cached Fox data
   dataF = responseF.json()
-  g = len(dataF)
-  h = random.randint(0,g)
-  images.append(dataF[u'articles'][h][u'urlToImage'])
-  titles.append(dataF[u'articles'][h][u'title'])
-  descriptions.append(dataF[u'articles'][h][u'description'])
-  urls.append(dataF[u'articles'][h][u'url'])
-  pub.append(dataF[u'articles'][h][u'publishedAt'].split('T')[0])
+  Fox = dataF[u'articles']
+  random.shuffle(Fox)
+  #Delete below
+  a = len(dataF)
+  b = random.randint(0,a)
+  images.append(dataF[u'articles'][b][u'urlToImage'])
+  titles.append(dataF[u'articles'][b][u'title'])
+  descriptions.append(dataF[u'articles'][b][u'description'])
+  urls.append(dataF[u'articles'][b][u'url'])
+  pub.append(dataF[u'articles'][b][u'publishedAt'].split('T')[0])
 
   #Brietbar API
   responseB = requests.get('https://newsapi.org/v1/articles?source=breitbart-news&sortBy=top&apiKey=3b606a8547b24dfdbf43e72388b6f539')
-  #cached Briet data
   dataB = responseB.json()
-  i = len(dataB)
-  j = random.randint(0,i)
-  images.append(dataB[u'articles'][j][u'urlToImage'])
-  titles.append(dataB[u'articles'][j][u'title'])
-  descriptions.append(dataB[u'articles'][j][u'description'])
-  urls.append(dataB[u'articles'][j][u'url'])
-  pub.append(dataB[u'articles'][j][u'publishedAt'].split('T')[0])
+  Brietbar = dataB[u'articles']
+  random.shuffle(Brietbar)
+  #Delete below
+  a = len(dataB)
+  b = random.randint(0,a)
+  images.append(dataB[u'articles'][b][u'urlToImage'])
+  titles.append(dataB[u'articles'][b][u'title'])
+  descriptions.append(dataB[u'articles'][b][u'description'])
+  urls.append(dataB[u'articles'][b][u'url'])
+  pub.append(dataB[u'articles'][b][u'publishedAt'].split('T')[0])
 
   #MTV News API
   responseM = requests.get('https://newsapi.org/v1/articles?source=mtv-news&sortBy=top&apiKey=3b606a8547b24dfdbf43e72388b6f539')
-  #cached MTV data
   dataM = responseM.json()
-  k = len(dataM)
-  l = random.randint(0,k)
-  images.append(dataM[u'articles'][l][u'urlToImage'])
-  titles.append(dataM[u'articles'][l][u'title'])
-  descriptions.append(dataM[u'articles'][l][u'description'])
-  urls.append(dataM[u'articles'][l][u'url'])
-  pub.append(dataM[u'articles'][l][u'publishedAt'].split('T')[0])
+  MTV = dataM[u'articles']
+  random.shuffle(MTV)
+  #Delete below
+  a = len(dataM)
+  b = random.randint(0,a)
+  images.append(dataM[u'articles'][b][u'urlToImage'])
+  titles.append(dataM[u'articles'][b][u'title'])
+  descriptions.append(dataM[u'articles'][b][u'description'])
+  urls.append(dataM[u'articles'][b][u'url'])
+  pub.append(dataM[u'articles'][b][u'publishedAt'].split('T')[0])
 
   #Recode API
   responseR = requests.get('https://newsapi.org/v1/articles?source=recode&sortBy=top&apiKey=3b606a8547b24dfdbf43e72388b6f539')
-  #cached Recode data
   dataR = responseR.json()
-  l = len(dataR)
-  x = random.randint(0,l)
-  images.append(dataR[u'articles'][x][u'urlToImage'])
-  titles.append(dataR[u'articles'][x][u'title'])
-  descriptions.append(dataR[u'articles'][x][u'description'])
-  urls.append(dataR[u'articles'][x][u'url'])
-  pub.append(dataR[u'articles'][x][u'publishedAt'].split('T')[0])
+  Recode = dataR[u'articles']
+  random.shuffle(Recode)
+  #Delete below
+  a = len(dataR)
+  b = random.randint(0,a)
+  images.append(dataR[u'articles'][b][u'urlToImage'])
+  titles.append(dataR[u'articles'][b][u'title'])
+  descriptions.append(dataR[u'articles'][b][u'description'])
+  urls.append(dataR[u'articles'][b][u'url'])
+  pub.append(dataR[u'articles'][b][u'publishedAt'].split('T')[0])
 
   #Sports Bible API
   responseS = requests.get('https://newsapi.org/v1/articles?source=the-sport-bible&sortBy=top&apiKey=3b606a8547b24dfdbf43e72388b6f539')
-  #cached Sport Bible data
   dataS = responseS.json()
-  l = len(dataS)
-  x = random.randint(0,l)
-  images.append(dataS[u'articles'][x][u'urlToImage'])
-  titles.append(dataS[u'articles'][x][u'title'])
-  descriptions.append(dataS[u'articles'][x][u'description'])
-  urls.append(dataS[u'articles'][x][u'url'])
-  pub.append(dataS[u'articles'][x][u'publishedAt'].split('T')[0])
+  sportsBible = dataS[u'articles']
+  random.shuffle(sportsBible)
+  #Delete below
+  a = len(dataS)
+  b = random.randint(0,a)
+  images.append(dataS[u'articles'][b][u'urlToImage'])
+  titles.append(dataS[u'articles'][b][u'title'])
+  descriptions.append(dataS[u'articles'][b][u'description'])
+  urls.append(dataS[u'articles'][b][u'url'])
+  pub.append(dataS[u'articles'][b][u'publishedAt'].split('T')[0])
 
   context = {
+    'sportsBible': sportsBible,
+    'Recode': Recode,
+    'MTV': MTV,
+    'Brietbar': Brietbar,
+    'Fox': Fox,
+    'Crunch': Crunch,
+    'EW': EW,
+    'Huff': Huff,
+    #Delete below
     'images': images,
     'titles': titles,
     'descriptions': descriptions,
